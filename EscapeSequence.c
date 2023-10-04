ff99766b0f032470742f166c1c874e33ad882f20
Shivam Preet <146970003+mantavyam@users.noreply.github.com>
2023-10-04 20:46:26 +0530

    Add files via upload

---
+++ EscapeSequence.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    // This is a single line comment
+    /* 
+    This is a 
+    Multi Line
+    Comment 
+    */
+   printf("\"Never Settle\" - OnePlus\n"); // If you need to include Double Quote
+   printf("\'Never Settle\' - OnePlus\n"); // If you need to include Single Quote
+   printf("\\Never Settle\\ - OnePlus\n"); // If you need to include Oblique
+   printf("A Small Matrix:\n1\t2\t3\n4\t5\t6\n7\t8\t9"); // This is how we can use tab escape sequence
+   return 0;
+}
---
+++ PrintStatement.c
@@ -0,0 +1,7 @@
+# include<stdio.h>
+int main()
+{
+    //This is a basic Print Statement Program written in C language
+    printf("I have installed VS Code and GCC");
+    return 0;
+}

d6dcfad68abc69b29681dae89075152fdfa5e9dc
Shivam Preet <146970003+mantavyam@users.noreply.github.com>
2023-10-04 21:42:10 +0530

    Add files via upload

---
+++ Variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int studentID = 004;             
+    float studentFees = 65000.0;       
+    char section = 'A';        
+    char studentName[] = "Shivam"; 
+    printf("Name of Student is %s\n", studentName);
+    printf("His ID is %d\n", studentID);
+    printf("He is in %c\n", section);
+    printf("He has given %f Rs as fees\n", studentFees);
+}

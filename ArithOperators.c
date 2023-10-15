49371d36da8fc592c246656efd7d031dc796940d
Shivam Preet <146970003+mantavyam@users.noreply.github.com>
2023-10-15 21:57:46 +0530

    Add files via upload

---
+++ ArithOperators.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(
+    int number1 = 16;
+    int number2 = 8;
+    v = number1 + number2;
+    w = number1 - number2;
+    x = number1 * number2;
+    y = number1 / number2;
+    printf("Sum of Both Numbers %d", v);
+    printf("Difference of Both Numbers %d", w);
+    printf("Multiplication of Both Numbers %d", x);
+    printf("Division of Both Numbers %d", y);
+    return 0;
+)

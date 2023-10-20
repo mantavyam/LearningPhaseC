87a79c5ef5c95578d2fa2b1ab1fefcadd0440bdd
Shivam Preet <146970003+mantavyam@users.noreply.github.com>
2023-10-20 18:20:55 +0530

    Update mathFn.c

--- mathFn.c
+++ mathFn.c
@@ -1,1 +1,27 @@
-
+// Important Math Operations
+#include <stdio.h>
+#include <math.h>
+int main() {
+    // Always Include math.h for maths
+    double A = sqrt(9);
+    double B = pow(2,4);
+    int C = round(3.14);
+    int D = ceil(4.4);
+    int E = floor(3.99);
+    double F = fabs(-100);
+     G = log(3);
+    double H = sin(90);
+    double I = cos(0);
+    double J = tan(45);
+    printf("\n%.2lf", A);
+    printf("\n%.2lf", B);
+    printf("\n%i", C);
+    printf("\n%i", D);
+    printf("\n%.2lf", E);
+    printf("\n%.2lf", F);
+    printf("\n%.2lf", G);
+    printf("\n%.2lf", H);
+    printf("\n%.2lf", I);
+    printf("\n%.2lf", J);
+    return 0;
+}

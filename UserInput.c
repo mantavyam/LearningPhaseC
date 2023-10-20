f850aa2ace754b101abf66874e6b87c8c394fc5f
Shivam Preet <146970003+mantavyam@users.noreply.github.com>
2023-10-20 17:26:54 +0530

    Update UserInput.c

--- UserInput.c
+++ UserInput.c
@@ -1,1 +1,24 @@
+// Program in C to accept UserInput
+#include <stdio.h>
 
+int main() {
+    // Variable Datatype define
+    int Age, Revenue;
+    char Name[10], Company_Name[10];
+    // Asking User for Input to our variables
+    printf("Mention your Name:");
+    scanf("%s", &Name);
+    printf("Mention your Company Name:");
+    scanf("%s", &Company_Name);
+    printf("Mention your Age:");
+    scanf("%i", &Age);
+    ("Mention your Revenue:");
+    scanf("%i", &Revenue);
+    // Display of our Variable Values
+    printf("\nProfile: %s",Name);
+    printf("\nCurrent Age: %i",Age);
+    printf("\nFounder of %s", Company_Name);
+    printf("\nRevenue Generated: %i",Revenue);
+
+    return 0;
+}

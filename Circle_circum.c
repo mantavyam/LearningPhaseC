8e56991a68c007affbb6ccdf5ffe53d8429f5b8c
Shivam Preet <146970003+mantavyam@users.noreply.github.com>
2023-10-21 21:33:39 +0530

    Update Circle_circum.c

--- Circle_circum.c
+++ Circle_circum.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    // Calculate Circumference of Circle
+    // Calculate Area of Circle
+    const double PI = 3.14159;
+    double radius, circumference, area;
+    printf("Enter the Radius of Circle:");
+    scanf("%lf", &radius);
+    circumference = 2 * PI * radius ;
+    area = PI * radius * radius;
+    printf("\nCircumference of Circle: %.2lf", circumference);
+    printf("\nArea of Circle: %.2lf", area);
+    return 0;
+}
